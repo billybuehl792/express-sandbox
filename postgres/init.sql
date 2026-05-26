8ccf8937e5f0de11106e4c5a2cadfe2c8f35393b
Billy Buehl <billybuehl792@gmail.com>
2026-05-26 12:19:32 -0400

    feat: initialize postgres

---
+++ postgres/init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS items (
+    id SERIAL PRIMARY KEY,
+    label TEXT NOT NULL,
+    description TEXT
+);
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,39 @@
 import "dotenv/config";
 import express from "express";
-import fs, { appendFile, appendFileSync } from "fs";
-import path from "path";
-
-const app = express();
-
-app.use(express.json());
+import { Pool } from "pg";
 
 const PORT = parseInt(process.env.PORT || "3000");
-const DATA_DIR = path.resolve("..", "data");
 
-app.get("/", (_req, res) => {
-  res.json({ message: "Hello from Docker + Express + TypeScript" });
+const POSTGRES_HOST = process.env.POSTGRES_HOST;
+const POSTGRES_USER = process.env.POSTGRES_USER;
+const POSTGRES_PORT = process.env.POSTGRES_PORT;
+const POSTGRES_DB = process.env.POSTGRES_DB;
+const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD;
+const POSTGRES_DB_URL = `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}`;
+
+const app = express();
+app.use(express.json());
+
+const db = new Pool({ connectionString: POSTGRES_DB_URL });
+
+app.get("/", async (_req, res) => {
+  const result = await db.query("SELECT * FROM items ORDER BY id ASC");
+  res.json({ results: result.rows });
 });
 
-app.get("/messages", (_req, res) => {
-  res.json({
-    message: "Hello from Docker + Express + TypeScript",
-  });
-});
+app.post("/", async (req, res) => {
+  const { label, description } = req.body;
 
-app.post("/messages/create", (req, res, next) => {
-  const text = req.body.text;
+  const result = await db.query(
+    `
+    INSERT INTO items (label, description)
+    VALUES ($1, $2)
+    RETURNING *
+    `,
+    [label, description],
+  );
 
-  if (typeof text !== "string" || text.trim() === "")
-    res.status(400).json({ message: "Body text required" });
-  else
-    appendFile(path.join(DATA_DIR, "messages.txt"), `${text}\n`, (error) => {
-      if (error) next(error);
-      else res.json({ data: text, success: true });
-    });
+  res.json(result.rows[0]);
 });
 
 app.listen(PORT, "0.0.0.0", () => {
